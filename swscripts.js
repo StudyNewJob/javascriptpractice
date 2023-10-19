3f09d3bcea24d6719a719179142da2e49f93f2bd
Shama-David Grier-Johnson <127712361+StudyNewJob@users.noreply.github.com>
2023-10-19 02:31:06 -0400

    Update swscripts.js

--- swscripts.js
+++ swscripts.js
@@ -1,9 +1,15 @@
+function pizzaCheck(like) {
+    if ((like == 'y') ||(like== 'Y') ) {
+        document.write ("You like pizza!");
+    } else if { (like == 'n') || (like == 'N')
+        document.write ("You don't like pizza?");
+    } else { document.write ("Invalid input");
+           }
+}
 alert("Hello! Welcome to my Star Wars fan page!");
 var name = prompt("What's your name?");
 document.write("Welcome ", name );
-if (confirm("Are you from the USA?")) {
-    document.write("Howdy Partner!");
-} else {
-    document.write("Welcome");
-}
 
+var likeP = prompt("Do you like pizza? Type y or Y for yes or N or n for no.")
+
+pizzaCheck(likeP);
