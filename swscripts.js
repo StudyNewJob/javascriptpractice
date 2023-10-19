22cdf1bff37c0fb29172dbdabd45a9c72d3ecd4d
Shama-David Grier-Johnson <127712361+StudyNewJob@users.noreply.github.com>
2023-10-19 02:19:19 -0400

    Update swscripts.js

--- swscripts.js
+++ swscripts.js
@@ -1,6 +1,6 @@
 alert("Hello! Welcome to my Star Wars fan page!");
 var name = prompt("What's your name?");
-document.write("Welcome ", name ");
+document.write("Welcome ", name );
 if (confirm("Are you from the USA?")) {
     document.write("Howdy Partner!");
 } else {
